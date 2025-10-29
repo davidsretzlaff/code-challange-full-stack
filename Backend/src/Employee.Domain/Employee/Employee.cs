3c9a83a18d4ec022e6377f1d40d0577044d12ad2
davidsretzlaff <davids.retzlaff@gmail.com>
2025-10-29 10:23:58 -0300

    feat: creating domain layer

---
+++ Backend/src/Employee.Domain/Employee/Employee.cs
@@ -0,0 +1,27 @@
+﻿namespace Employee.Domain.Employee
+{
+    public sealed class Employee
+    {
+        public Employee(string firstName, string lastName, string email, string position)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Position = position;
+        }
+
+        public int Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Email { get; private set; } 
+        public string Position { get; private set; }
+
+        public static Employee Create(string firstName, string lastName, string email, string position) 
+        {
+            // validations
+            var employee = new Employee(firstName, lastName, email, position);
+            return employee;
+        }
+
+    }
+}
